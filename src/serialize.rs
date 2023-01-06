17d76fb1b540f302f5919683fdbbb313ea093d07
Martijn Faassen <faassen@startifact.com>
2023-01-06 14:26:59 +0100

    Start to rewrite serializer so we can adjust how serialization works

--- src/serialize.rs
+++ src/serialize.rs
@@ -6,7 +6,7 @@ use crate::error::Error;
 use crate::fullname::FullnameSerializer;
 use crate::namespace::NamespaceId;
 use crate::prefix::PrefixId;
-use crate::xmlvalue::{ToNamespace, Value, ValueType};
+use crate::xmlvalue::{Element, ToNamespace, Value, ValueType};
 use crate::xotdata::{Node, Xot};
 
 /// ## Serialization
@@ -126,26 +126,6 @@ impl<'a> Xot<'a> {
         self.serialize_node_helper(node, w, ToNamespace::new())
     }
 
-    fn serialize_node_helper(
-        &self,
-        node: Node,
-        w: &mut impl Write,
-        to_namespace: ToNamespace,
-    ) -> Result<(), Error> {
-        let mut fullname_serializer = FullnameSerializer::with_to_namespace(to_namespace, self);
-        for edge in self.traverse(node) {
-            match edge {
-                NodeEdge::Start(current_node) => {
-                    self.handle_edge_start(node, current_node, w, &mut fullname_serializer)?;
-                }
-                NodeEdge::End(current_node) => {
-                    self.handle_edge_end(current_node, w, &mut fullname_serializer)?;
-                }
-            }
-        }
-        Ok(())
-    }
-
     /// Serialize document to a string.
     ///
     /// Like [`Xot::serialize_or_missing_prefix`], but returns a string instead of writing to a writer.
@@ -173,61 +153,86 @@ impl<'a> Xot<'a> {
         String::from_utf8(buf).unwrap()
     }
 
+    fn serialize_node_helper(
+        &self,
+        node: Node,
+        w: &mut impl Write,
+        to_namespace: ToNamespace,
+    ) -> Result<(), Error> {
+        let mut fullname_serializer = FullnameSerializer::with_to_namespace(to_namespace, self);
+        let mut xml_writer = XmlSerializerWriter::new(self, &mut fullname_serializer, w);
+
+        for edge in self.traverse(node) {
+            match edge {
+                NodeEdge::Start(current_node) => {
+                    self.handle_edge_start(node, current_node, &mut xml_writer)?;
+                }
+                NodeEdge::End(current_node) => {
+                    self.handle_edge_end(current_node, &mut xml_writer)?;
+                }
+            }
+        }
+        Ok(())
+    }
+
     fn handle_edge_start(
         &self,
         top_node: Node,
         node: Node,
-        w: &mut impl Write,
-        fullname_serializer: &mut FullnameSerializer,
+        xml_writer: &mut XmlSerializerWriter<'a, impl Write>,
     ) -> Result<(), Error> {
         let value = self.value(node);
         match value {
             Value::Root => {}
             Value::Element(element) => {
-                fullname_serializer.push(&element.namespace_info.to_namespace);
+                xml_writer.write_start_tag_open(node, element)?;
 
-                let fullname = fullname_serializer.fullname_or_err(element.name_id)?;
-
-                write!(w, "<{}", fullname)?;
                 // serialize any extra prefixes if this is the top element of
                 // a fragment and they aren't declared already
                 if node == top_node {
-                    for (prefix_id, namespace_id) in fullname_serializer.top_to_namespace().iter() {
-                        if !element.namespace_info.to_namespace.contains_key(prefix_id) {
-                            self.write_namespace_declaration(*prefix_id, *namespace_id, w)?;
+                    let extra_prefixes = xml_writer
+                        .extra_prefixes()
+                        .iter()
+                        .map(|(prefix_id, namespace_id)| (*prefix_id, *namespace_id))
+                        .collect::<Vec<_>>();
+                    for (prefix_id, namespace_id) in extra_prefixes {
+                        if !element.namespace_info.to_namespace.contains_key(&prefix_id) {
+                            xml_writer.write_space()?;
+                            xml_writer.write_namespace_declaration(
+                                node,
+                                element,
+                                prefix_id,
+                                namespace_id,
+                            )?;
                         }
                     }
                 }
-                for (prefix_id, namespace_id) in element.namespace_info.to_namespace.iter() {
-                    self.write_namespace_declaration(*prefix_id, *namespace_id, w)?;
-                }
-                for (name_id, value) in element.attributes.iter() {
-                    let fullname = fullname_serializer.fullname_attr_or_err(*name_id)?;
-                    write!(w, " {}=\"{}\"", fullname, serialize_attribute(value.into()))?;
+
+                for (prefix_id, namespace_id) in element.prefixes() {
+                    xml_writer.write_space()?;
+                    xml_writer.write_namespace_declaration(
+                        node,
+                        element,
+                        *prefix_id,
+                        *namespace_id,
+                    )?;
                 }
 
-                if self.first_child(node).is_none() {
-                    write!(w, "/>")?;
-                } else {
-                    write!(w, ">")?;
+                for (name_id, value) in element.attributes() {
+                    xml_writer.write_space()?;
+                    xml_writer.write_attribute(node, element, *name_id, value)?;
                 }
+
+                xml_writer.write_start_tag_close(node, element)?;
             }
             Value::Text(text) => {
-                write!(w, "{}", serialize_text(text.get().into()))?;
+                xml_writer.write_text(node, text)?;
             }
             Value::Comment(comment) => {
-                write!(w, "<!--{}-->", comment.get())?;
+                xml_writer.write_comment(node, comment)?;
             }
             Value::ProcessingInstruction(pi) => {
-                if let Some(data) = pi.data() {
-                    if !data.is_empty() {
-                        write!(w, "<?{} {}?>", pi.target(), data)?;
-                    } else {
-                        write!(w, "<?{}?>", pi.target())?;
-                    }
-                } else {
-                    write!(w, "<?{}?>", pi.target())?;
-                }
+                xml_writer.write_processing_instruction(node, pi)?;
             }
         }
         Ok(())
@@ -236,37 +241,258 @@ impl<'a> Xot<'a> {
     fn handle_edge_end(
         &self,
         node: Node,
-        w: &mut impl Write,
-        fullname_serializer: &mut FullnameSerializer,
+        xml_writer: &mut XmlSerializerWriter<'a, impl Write>,
     ) -> Result<(), Error> {
         let value = self.value(node);
         if let Value::Element(element) = value {
-            if self.first_child(node).is_some() {
-                let fullname = fullname_serializer.fullname_or_err(element.name_id)?;
-                write!(w, "</{}>", fullname)?;
-            }
-            fullname_serializer.pop(&element.namespace_info.to_namespace);
+            xml_writer.write_end_tag(node, element)?;
+        }
+        Ok(())
+    }
+}
+
+use crate::name::NameId;
+use crate::xmlvalue::{Comment, ProcessingInstruction, Text};
+
+pub trait SerializerWriter {
+    fn extra_prefixes(&self) -> &ToNamespace;
+    fn write_start_tag_open(&mut self, node: Node, element: &Element) -> Result<(), Error>;
+    fn write_start_tag_close(&mut self, node: Node, element: &Element) -> Result<(), Error>;
+    fn write_end_tag(&mut self, node: Node, element: &Element) -> Result<(), Error>;
+    fn write_namespace_declaration(
+        &mut self,
+        node: Node,
+        element: &Element,
+        prefix_id: PrefixId,
+        namespace_id: NamespaceId,
+    ) -> Result<(), Error>;
+    fn write_attribute(
+        &mut self,
+        node: Node,
+        element: &Element,
+        name_id: NameId,
+        value: &str,
+    ) -> Result<(), Error>;
+    fn write_text(&mut self, node: Node, text: &Text) -> Result<(), Error>;
+    fn write_comment(&mut self, node: Node, comment: &Comment) -> Result<(), Error>;
+    fn write_processing_instruction(
+        &mut self,
+        node: Node,
+        pi: &ProcessingInstruction,
+    ) -> Result<(), Error>;
+    fn write_space(&mut self) -> Result<(), Error>;
+}
+
+struct XmlSerializerWriter<'a, W: Write> {
+    xot: &'a Xot<'a>,
+    fullname_serializer: &'a mut FullnameSerializer<'a>,
+    w: &'a mut W,
+}
+
+impl<'a, W: Write> XmlSerializerWriter<'a, W> {
+    pub(crate) fn new(
+        xot: &'a Xot<'a>,
+        fullname_serializer: &'a mut FullnameSerializer<'a>,
+        w: &'a mut W,
+    ) -> XmlSerializerWriter<'a, W> {
+        XmlSerializerWriter {
+            xot,
+            fullname_serializer,
+            w,
+        }
+    }
+}
+
+impl<'a, W: Write> SerializerWriter for XmlSerializerWriter<'a, W> {
+    fn extra_prefixes(&self) -> &ToNamespace {
+        self.fullname_serializer.top_to_namespace()
+    }
+
+    fn write_start_tag_open(&mut self, node: Node, element: &Element) -> Result<(), Error> {
+        self.fullname_serializer
+            .push(&element.namespace_info.to_namespace);
+        write!(
+            self.w,
+            "<{}",
+            self.fullname_serializer.fullname_or_err(element.name())?
+        )?;
+        Ok(())
+    }
+
+    fn write_start_tag_close(&mut self, node: Node, element: &Element) -> Result<(), Error> {
+        if self.xot.first_child(node).is_none() {
+            write!(self.w, "/>")?;
+        } else {
+            write!(self.w, ">")?;
         }
         Ok(())
     }
 
+    fn write_end_tag(&mut self, node: Node, element: &Element) -> Result<(), Error> {
+        if self.xot.first_child(node).is_some() {
+            let fullname = self.fullname_serializer.fullname_or_err(element.name())?;
+            write!(self.w, "</{}>", fullname)?;
+        }
+        self.fullname_serializer
+            .pop(&element.namespace_info.to_namespace);
+        Ok(())
+    }
+
     fn write_namespace_declaration(
-        &self,
+        &mut self,
+        node: Node,
+        element: &Element,
         prefix_id: PrefixId,
         namespace_id: NamespaceId,
-        w: &mut impl Write,
     ) -> Result<(), Error> {
-        let namespace = self.namespace_lookup.get_value(namespace_id);
-        if prefix_id == self.empty_prefix_id {
-            write!(w, " xmlns=\"{}\"", namespace)?;
+        let namespace = self.xot.namespace_str(namespace_id);
+        if prefix_id == self.xot.empty_prefix_id {
+            write!(self.w, "xmlns=\"{}\"", namespace)?;
         } else {
             write!(
-                w,
-                " xmlns:{}=\"{}\"",
-                self.prefix_lookup.get_value(prefix_id),
+                self.w,
+                "xmlns:{}=\"{}\"",
+                self.xot.prefix_str(prefix_id),
                 namespace
             )?;
         }
         Ok(())
     }
+
+    fn write_attribute(
+        &mut self,
+        node: Node,
+        element: &Element,
+        name_id: NameId,
+        value: &str,
+    ) -> Result<(), Error> {
+        let fullname = self.fullname_serializer.fullname_attr_or_err(name_id)?;
+        write!(
+            self.w,
+            "{}=\"{}\"",
+            fullname,
+            serialize_attribute(value.into())
+        )?;
+        Ok(())
+    }
+
+    fn write_text(&mut self, node: Node, text: &Text) -> Result<(), Error> {
+        write!(self.w, "{}", serialize_text(text.get().into()))?;
+        Ok(())
+    }
+
+    fn write_comment(&mut self, node: Node, comment: &Comment) -> Result<(), Error> {
+        write!(self.w, "<!--{}-->", comment.get())?;
+        Ok(())
+    }
+
+    fn write_processing_instruction(
+        &mut self,
+        node: Node,
+        processing_instruction: &ProcessingInstruction,
+    ) -> Result<(), Error> {
+        if let Some(data) = processing_instruction.data() {
+            if !data.is_empty() {
+                write!(self.w, "<?{} {}?>", processing_instruction.target(), data)?;
+            } else {
+                write!(self.w, "<?{}?>", processing_instruction.target())?;
+            }
+        } else {
+            write!(self.w, "<?{}?>", processing_instruction.target())?;
+        }
+        Ok(())
+    }
+
+    fn write_space(&mut self) -> Result<(), Error> {
+        write!(self.w, " ")?;
+        Ok(())
+    }
+}
+
+fn write_start_tag_open(
+    fullname_serializer: &FullnameSerializer,
+    element: &Element,
+    w: &mut impl Write,
+) -> Result<(), Error> {
+    write!(
+        w,
+        "<{}",
+        fullname_serializer.fullname_or_err(element.name())?
+    )?;
+    Ok(())
+}
+
+fn write_start_tag_close(xot: &Xot, node: Node, w: &mut impl Write) -> Result<(), Error> {
+    if xot.first_child(node).is_none() {
+        write!(w, "/>")?;
+    } else {
+        write!(w, ">")?;
+    }
+    Ok(())
+}
+
+fn write_end_tag(
+    xot: &Xot,
+    fullname_serializer: &FullnameSerializer,
+    node: Node,
+    element: &Element,
+    w: &mut impl Write,
+) -> Result<(), Error> {
+    if xot.first_child(node).is_some() {
+        let fullname = fullname_serializer.fullname_or_err(element.name())?;
+        write!(w, "</{}>", fullname)?;
+    }
+    Ok(())
+}
+
+fn write_namespace_declaration(
+    xot: &Xot,
+    prefix_id: PrefixId,
+    namespace_id: NamespaceId,
+    w: &mut impl Write,
+) -> Result<(), Error> {
+    let namespace = xot.namespace_str(namespace_id);
+    if prefix_id == xot.empty_prefix_id {
+        write!(w, "xmlns=\"{}\"", namespace)?;
+    } else {
+        write!(w, "xmlns:{}=\"{}\"", xot.prefix_str(prefix_id), namespace)?;
+    }
+    Ok(())
+}
+
+fn write_attribute(
+    fullname_serializer: &FullnameSerializer,
+    name_id: NameId,
+    value: &str,
+    w: &mut impl Write,
+) -> Result<(), Error> {
+    let fullname = fullname_serializer.fullname_attr_or_err(name_id)?;
+    write!(w, "{}=\"{}\"", fullname, serialize_attribute(value.into()))?;
+    Ok(())
+}
+
+fn write_text(text: &Text, w: &mut impl Write) -> Result<(), Error> {
+    write!(w, "{}", serialize_text(text.get().into()))?;
+    Ok(())
+}
+
+fn write_comment(comment: &Comment, w: &mut impl Write) -> Result<(), Error> {
+    write!(w, "<!--{}-->", comment.get())?;
+    Ok(())
+}
+
+fn write_processing_instruction(
+    processing_instruction: &ProcessingInstruction,
+    w: &mut impl Write,
+) -> Result<(), Error> {
+    if let Some(data) = processing_instruction.data() {
+        if !data.is_empty() {
+            write!(w, "<?{} {}?>", processing_instruction.target(), data)?;
+        } else {
+            write!(w, "<?{}?>", processing_instruction.target())?;
+        }
+    } else {
+        write!(w, "<?{}?>", processing_instruction.target())?;
+    }
+    Ok(())
 }
